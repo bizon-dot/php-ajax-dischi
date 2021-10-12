fb991b7cb7429bf0fefc87e1d0f6cefb908cccb1
bizoon <r.aureli@protonmail.com>
2021-10-12 15:05:13 +0100

    Struttura flexbox

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhpDISK</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "database.php" ?>
+    <div class="container">
+        <div class="container-album">
+            <!-- <div class="album">
+                1
+            </div>
+            <div class="album">
+                2
+            </div>
+            <div class="album">
+                3
+            </div>
+            <div class="album">
+                4
+            </div>
+            <div class="album">
+                5 -->
+            </div>
+        </div>
+        
+    </div>
+    
+</body>
+</html>
