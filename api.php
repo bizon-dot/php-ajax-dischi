ed13d5d74c4c8b010ccffcc3e2c3682506cd0e85
bizoon <r.aureli@protonmail.com>
2021-10-13 16:56:37 +0100

    Passo il parametro sezionato tramite una richiesta GET a api.php

--- api.php
+++ api.php
@@ -1,11 +1,29 @@
 <?php
+function query ($str,$db){
+    $filterdAlbum = [];
+    foreach ($db as $album) {
+        ($album["genere"] == $str) ? array_push($filterdAlbum,$album) : null;
+    }
+    print_r($filterdAlbum);
+    return $filterdAlbum;
+}
+
+
 
 require __DIR__ . "/database.php";
 
 
-header('Content-Type: application/json');
+if (isset($_GET["query"])) {
+    header('Content-Type: application/json');
+    echo (json_encode($db));
 
-echo (json_encode($db));
+} else {
+     header('Content-Type: application/json');
+     echo (json_encode($db));
+}
+
+
+
 
 
 
